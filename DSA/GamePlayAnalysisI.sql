03a6da666c4e350739bc91be17a79f2b4d3b5911
prasannaprabhud <prabhuverse1@gmail.com>
2025-11-29 13:50:27 +0000

    :memo: Added DSA Problen sol

---
+++ DSA/GamePlayAnalysisI.sql
@@ -0,0 +1,7 @@
+-- 511. Game Play Analysis I
+-- https://leetcode.com/problems/game-play-analysis-i?envType=problem-list-v2&envId=vkvhgcwg
+
+-- /* Write your PL/SQL query statement below */
+select player_id, to_char(min(event_date), 'YYYY-MM-DD') as first_login
+from activity
+group by player_id;
